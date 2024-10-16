6a4dd13d2c2ae02643cdde7cb1b53a8bdc82c93e
Prathmesh Borade <125908123+pborade90@users.noreply.github.com>
2024-10-16 13:52:31 +0530

    Changes in API calls

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -4,7 +4,7 @@ const key = "7gPXOXrtoieajqIsoI6c72LYIQJVaXip"
 
 const getWeather = async id => {
 
-  const baseURL = "http://dataservice.accuweather.com/currentconditions/v1/"
+  const baseURL = "https://dataservice.accuweather.com/currentconditions/v1/"
   const query = `${id}?apikey=${key}`
 
   const response = await fetch(baseURL + query)
@@ -20,7 +20,7 @@ const getWeather = async id => {
 
 const getCity = async city => {
 
-  const baseURL = "http://dataservice.accuweather.com/locations/v1/cities/search"
+  const baseURL = "https://dataservice.accuweather.com/locations/v1/cities/search"
   const query = `?apikey=${key}&q=${city}`
 
   const response = await fetch(baseURL + query)
